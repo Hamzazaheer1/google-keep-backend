e61ebf7152584e0ade365b32a09081268a67b377
Hamza Zaheer <malikhamza1619619@gmail.com>
2023-06-21 14:25:08 +0500

    base code

--- app.js
+++ app.js
@@ -16,18 +16,13 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "https://main--celebrated-cat-6772f5.netlify.app",
-    ],
+    origin: ["http://localhost:5173"],
   })
 );
 
 app.options("*", cors());
 
 app.use(require("./router/userRoute"));
-app.use(require("./router/managerRoute"));
-app.use(require("./router/waiterRoute"));
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
